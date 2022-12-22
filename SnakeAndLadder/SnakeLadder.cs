491c48f0a6b218a0e9f753ecfb8280725e47a28e
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-23 00:28:32 +0530

    RolDie added in program

--- SnakeAndLadder/SnakeLadder.cs
+++ SnakeAndLadder/SnakeLadder.cs
@@ -13,6 +13,12 @@ namespace SnakeAndLadder
             //UC1 - single player at start position 0
             int position = 0;
             Console.WriteLine("Player is at {0} position.", position);
+            //UC2 - The Player rolls the die to get a number between 1 to 6.
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            position = position + die;
+            Console.WriteLine("You Got : " + die);
+            Console.WriteLine("Player position at : "+position);
         }
     }
 }
