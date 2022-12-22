d5394ca155f8a952209e5dac7f7e8d6bc8b9cda6
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-23 00:59:05 +0530

    get Exact position 100 condition added

--- SnakeAndLadder/SnakeLadder.cs
+++ SnakeAndLadder/SnakeLadder.cs
@@ -11,19 +11,15 @@ namespace SnakeAndLadder
         public static void StartGame()
         {
             //UC1 - single player at start position 0
-            int position = 0;
+            int position = 0, die;
             Console.WriteLine("Player is at {0} position.", position);
-
-            //UC2 - The Player rolls the die to get a number between 1 to 6.
             Random random = new Random();
-            int die = random.Next(1, 7);
-            position = position + die;
-            Console.WriteLine("You Got : " + die);
-            Console.WriteLine("Player is at {0} position",position);
             
             //UC4 - Repeat till Position 100
             while (position < 100)
             {
+                //UC2 - The Player rolls the die to get a number between 1 to 6.
+                die = random.Next(1, 7);
                 //UC3 - Adding option for Ladder, Snake and No Play
                 int option = random.Next(0, 3);
                 switch (option)
@@ -35,6 +31,11 @@ namespace SnakeAndLadder
                     case 1: //ladder
                         Console.WriteLine("It's a Ladder with {0} points", die);
                         position = position + die; //move ahead by points
+                        //UC5 - Ensure the player gets exact winning position 100.
+                        if (position > 100)
+                        {
+                            position = position - die;
+                        }
                         break;
                     default: //No Play
                         Console.WriteLine("It's a no Play, No Points");
